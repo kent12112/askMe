1286ebc6a5cacbe3c94b5813d4f4ce9cd752a7df
kent12112 <kentoyanagishita@gmail.com>
2024-06-07 20:16:08 -0700

    fix the error

--- src/components/EditorOutput.tsx
+++ src/components/EditorOutput.tsx
@@ -27,7 +27,7 @@ const renderers = {
 
 const EditorOutput: FC<EditorOutputProps> = ({content}) => {
   return (
-    //@ts-expect-error
+    //@ts-ignore
   <Output 
     data={content}
     style={style} 
